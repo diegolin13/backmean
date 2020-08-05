8b567635c26a8a29d0dd6d12157b71ab3076abac
Diego Juarez <diegoazulyoro13@gmail.com>
2020-08-05 18:56:22 -0500

    dhashsa

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 require('../config');
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.createUser = (req, res, next) => {
